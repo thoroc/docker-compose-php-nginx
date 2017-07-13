8278f5ecf0aa1292cf48dfeb0c7c63bf9c8571dd
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2017-07-13 13:39:31 +0100

    3 sites working

--- src/site2/index.php
+++ src/site2/index.php
@@ -1,10 +1,11 @@
 <?php
 
 echo '
-<a href="/site1">site one</a> <a href="/site2">site two</a> <a href="/site3">site three</a>
+<a href="/site1/">site one</a> 
+<a href="/site2/">site two</a> 
+<a href="/site3/">site three</a>
 </br>
 </br>
 ';
-
 
 echo 'Site Two';
